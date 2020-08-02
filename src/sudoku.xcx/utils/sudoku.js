64504349dccdf4b02a920f3d51e1dfc4c874c7c3
Oliver <591402213@qq.com>
2020-08-02 13:39:48 +0800

    完成基本布局

--- src/sudoku.xcx/utils/sudoku.js
+++ src/sudoku.xcx/utils/sudoku.js
@@ -1,41 +1,38 @@
 class sudoku {
 
     onRefreshSudoku;
+    selectIndex;
 
     constructor(question, answer, onRefreshSudoku) {
         this.question = question;
         this.answer = answer;
         this.onRefreshSudoku = onRefreshSudoku;
+        this.selectIndex = -1;
         this.initData();
     }
-
     initData() {
         let _bindItemList = [];
 
         let _question = this.question;
+
         for (let i = 0; i < _question.length; i++) {
             let colIndex = i % 9;
             let rowIndex = Math.floor(i / 9);
-            let largeBlockIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
-            let middleBlockIndex = (rowIndex % 3) * 3 + (colIndex % 3);
-            // console.log(`i:${i}\t colIndex:${colIndex}\t rowIndex:${rowIndex}\t largeBlockIndex:${largeBlockIndex}\t middleBlockIndex:${middleBlockIndex}`);
-            if (!_bindItemList[largeBlockIndex])
-                _bindItemList[largeBlockIndex] = [];
-            if (!_bindItemList[largeBlockIndex][middleBlockIndex])
-                _bindItemList[largeBlockIndex][middleBlockIndex] = {};
+            let blockIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
+            let value = _question[i];
+            let index = i;
 
-            _bindItemList[largeBlockIndex][middleBlockIndex].value = _question[i];
-            _bindItemList[largeBlockIndex][middleBlockIndex].index = i;
+            _bindItemList[i] = { colIndex, rowIndex, blockIndex, value, index };
+
         }
         this.bindItemList = _bindItemList;
     }
-
 
     setValue(index, number) {
         this.onRefreshSudoku(index, number);
     }
 
-    addNote(index, number) {}
+    addNote(index, number) { }
 
 }
 export default sudoku
