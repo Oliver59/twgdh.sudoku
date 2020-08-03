3a5d1b7809f1ea685c278c75d194921f30fcc7f8
Oliver <591402213@qq.com>
2020-08-03 10:28:07 +0800

    完成部分功能

--- src/sudoku.xcx/pages/sudoku/sudoku.js
+++ src/sudoku.xcx/pages/sudoku/sudoku.js
@@ -13,64 +13,71 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         let q = "000692700907045000025000060380200076002706300670003058040000690000460807006328000";
         let a = "134692785967845132825137964381954276452786319679213458248571693513469827796328541";
-        var sudoku = new sudokuClass(q, a, this.onRefreshSudoku);
-        sudoku.setValue(0, "3");
-        this.setData({ sudoku });
+        new sudokuClass(q, a, this.onRefreshSudoku);
     },
 
 
-    onRefreshSudoku(index, number) {
-        console.log(arguments);
+    onRefreshSudoku(sudoku) {
+        this.setData({ sudoku });
+    },
+
+    onClickBlock(e) {
+        let item = e.currentTarget.dataset.item;
+        this.data.sudoku.setSelect(item.index);
+    },
+    onClickNumber(e) {
+        let number = e.currentTarget.dataset.number;
+        this.data.sudoku.clickNumber(number);
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     }
 });
--- src/sudoku.xcx/utils/sudoku.js
+++ src/sudoku.xcx/utils/sudoku.js
@@ -2,13 +2,19 @@ class sudoku {
 
     onRefreshSudoku;
     selectIndex;
+    bindItemList;
+    openNote;
+
 
     constructor(question, answer, onRefreshSudoku) {
         this.question = question;
         this.answer = answer;
         this.onRefreshSudoku = onRefreshSudoku;
         this.selectIndex = -1;
+        this.openNote = false;
         this.initData();
+        this.initNumber();
+        this.refreshList();
     }
     initData() {
         let _bindItemList = [];
@@ -21,11 +27,45 @@ class sudoku {
             let blockIndex = Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3);
             let value = _question[i];
             let index = i;
-
-            _bindItemList[i] = { colIndex, rowIndex, blockIndex, value, index };
-
+            let notes = [];
+            for (let j = 1; j < 10; j++) {
+                notes.push({ show: false, value: j });
+            }
+            let isSystem = value > 0;
+            _bindItemList[i] = { colIndex, rowIndex, blockIndex, value, index, notes, isSystem };
         }
         this.bindItemList = _bindItemList;
+    }
+
+    initNumber() {
+        let numbers = [];
+        for (let i = 1; i < 10; i++) {
+            let show = this.bindItemList.filter(c => c.value == i).length < 9;
+            numbers.push({ show, value: i });
+        }
+        this.numbers = numbers;
+    }
+
+    clickNumber(number) {
+        console.log('clickNumber', number);
+        if (!number.show) return;
+        console.log('clickNumber', number);
+        let selectItem = this.bindItemList[this.selectIndex];
+        if (selectItem.isSystem) return;
+        console.log('clickNumber', number);
+        if (this.openNote) {
+            //注释
+            selectItem.notes.forEach(c => {
+                if (c.value === number.value) c.show = !c.show;
+            });
+        } else {
+            //填写
+            selectItem.value = number.value;
+            console.log("填写！");
+        }
+        this.bindItemList[this.selectIndex] = selectItem;
+        this.initNumber();
+        this.refreshList();
     }
 
     setValue(index, number) {
@@ -33,6 +73,34 @@ class sudoku {
     }
 
     addNote(index, number) { }
+
+    setSelect(index) {
+        this.selectIndex = index;
+        this.refreshList();
+    }
+
+    refreshList() {
+        if (this.selectIndex >= 0) {
+            let selectedItem = this.bindItemList[this.selectIndex];
+            this.bindItemList.forEach(item => {
+                item.sameBlockClass = "";
+                item.sameNumberClass = "";
+                item.selectedClass = "";
+
+                if (item.rowIndex == selectedItem.rowIndex ||
+                    item.colIndex == selectedItem.colIndex ||
+                    item.blockIndex == selectedItem.blockIndex
+                ) {
+                    item.sameBlockClass = "sameBlock";
+                }
+                if (item.value != 0 && item.value == selectedItem.value) {
+                    item.sameBlockClass = "sameNumber";
+                }
+            });
+            selectedItem.selectedClass = "selected";
+        }
+        this.onRefreshSudoku(this);
+    }
 
 }
 export default sudoku
